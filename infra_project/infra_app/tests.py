e9a1180665ab4ea89a709e2c16904687ae01d29d
ArturioNe <artur.negovora@yandex.ru>
2021-09-12 21:59:59 +0300

    refact tests

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -1,22 +1,26 @@
 from http import HTTPStatus
+from django.urls import reverse
 
 from django.test import Client, TestCase
 
 
 class StaticPagesURLTests(TestCase):
-    def setUp(self):
-        self.guest_client = Client()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.guest_client = Client()
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
-        response = self.guest_client.get('index')
+        response = StaticPagesURLTests.guest_client.get(reverse('index'))
         self.assertEqual(response.status_code, HTTPStatus.OK)
         response = self.guest_client.get('second_page')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_context(self):
         """Проверка контекста страниц."""
-        response = self.guest_client.get('index')
+        response = self.guest_client.get(reverse('index'))
         self.assertContains(response, 'У меня получилось!')
-        response = self.guest_client.get('second_page')
+        response = self.guest_client.get(reverse('second_page'))
         self.assertContains(response, 'А это вторая страница!')
