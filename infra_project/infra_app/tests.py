386e34e94a0d32d60297006dcec01494b43ff55c
ArturioNe <artur.negovora@yandex.ru>
2021-09-13 20:44:33 +0300

    add tests

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -1,26 +1,21 @@
 from http import HTTPStatus
-from django.urls import reverse
-
 from django.test import Client, TestCase
 
 
 class StaticPagesURLTests(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
-        cls.guest_client = Client()
+    def setUp(self):
+        self.guest_client = Client()
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
-        response = StaticPagesURLTests.guest_client.get(reverse('index'))
+        response = self.guest_client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        response = self.guest_client.get('second_page')
+        response = self.guest_client.get('/second/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_context(self):
         """Проверка контекста страниц."""
-        response = self.guest_client.get(reverse('index'))
+        response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
-        response = self.guest_client.get(reverse('second_page'))
-        self.assertContains(response, 'А это вторая страница!')
+        response = self.guest_client.get('/second/')
+        self.assertContains(response, 'А это вторая страница')
